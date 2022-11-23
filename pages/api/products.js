36def0aa99960092e99b14970a19f7b6608bd3a4
gdennis97 <114692804+gdennis97@users.noreply.github.com>
2022-11-22 18:08:14 -0600

    11/22/22

--- pages/api/products.js
+++ pages/api/products.js
@@ -4,3 +4,4 @@ export default async function handle(req, res) {
   const Prof = await prisma.Prof.findMany();
   res.json(Prof);
 }
+
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,3 @@
-// import Register from './Register';
-// import Login from './Login';
-
 import React, { useState } from "react";
 import { Dropdown, Selection } from "react-dropdown-now";
 import "react-dropdown-now/style.css";
@@ -16,8 +13,8 @@ import Admin from "pages/admin";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 import styles from "../styles/index.module.css";
-import { isJsxClosingFragment } from "typescript";
-import { PrismaClient } from "@prisma/client";
+
+
 
 const locales = {
   "en-US": require("date-fns/locale/en-US"),
@@ -84,10 +81,18 @@ function App() {
     setPassword("");
   }
 
-  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+const campus = [{
+  campuses: "RP A-Wing"
+}]
+
+  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: ""});
   const [allEvents, setAllEvents] = useState(events);
 
+  // const [newCampus, setNewCampus] = useState(campuses);
+  // const [allCampus, setAllCampus] = useState(campus)
+
   function handleAddEvent() {
+    alert("Your Cubicle Has Been Booked, an email will be sent");
     for (let i = 0; i < allEvents.length; i++) {
       const d1 = new Date(allEvents[i].start);
       const d2 = new Date(newEvent.start);
@@ -109,13 +114,8 @@ function App() {
     setAllEvents([...allEvents, newEvent]);
   }
 
-  const HandleAlert = () => {
-    alert("Your Cubicle Has Been Booked, an email will be sent");
-  };
-
-
   const updatedCampus = async () => await prisma.Prof.update({
-    where: { email: "gdennis@gmail.com" },
+    where: { email: "gdennis@icstars.org" },
       data: {
         profile: {
           update: {
@@ -124,6 +124,8 @@ function App() {
       },
     },
   });
+
+
 
   if (loggedIn === false) {
     return (
@@ -159,11 +161,17 @@ function App() {
           <div className={styles.logo}></div>
           <div className={styles.pickdate}>Select a date</div>
           <div style={{ textAlign: "center" }}>
+          <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
             <DatePicker
               placeholderText="Click here to select"
+                  popperPlacement="top-start"
               selected={newEvent.start}
               onChange={(start) => setNewEvent({ ...newEvent, start })}
+                  isClearable 
+                  style={{ marginLeft: 335}}
+                  withPortal
             />
+            <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} isClearable withPortal/>
             <Calendar className={styles.calendar}
               localizer={localizer}
               events={allEvents}
@@ -171,11 +179,8 @@ function App() {
               endAccessor="end"
               style={{ height: 300, width: 600, marginLeft: 375, marginTop: 15 }}
             />
-            <div style={{ textAlign: "center" }}>
-              {/* <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} /> */}
 
-              {/* <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} /> */}
-              {/* <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}> Add Event </button>  */}
+            <div>
               <Dropdown
                 className={styles.dropdown}
                 placeholder="Click here to select your Campus"
@@ -219,9 +224,7 @@ function App() {
               />
             </div>
             <div>
-              <button className={styles.Book} onClick={HandleAlert}>
-                Book
-              </button>
+            <button className={styles.Book} onClick={handleAddEvent}> Book </button>
             </div>
           </div>
         </div>
